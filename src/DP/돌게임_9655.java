fad1e160a1814474f321e95afd93bed1ffcce660
holychan <east0321@outlook.kr>
2025-03-18 02:16:03 +0900

    upload: 돌게임_9655

---
+++ src/DP/돌게임_9655.java
@@ -0,0 +1,22 @@
+package src.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 돌게임_9655 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if(n % 2 == 0){
+            System.out.println("CY");
+        } else {
+            System.out.println("SK");
+        }
+
+
+
+    }
+}
